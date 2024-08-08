c7c791b1b9fbf7d30a209ef8fc582d956371536f
topi0247 <karubona.ra247@gmail.com>
2024-08-08 18:45:58 +0900

    feat:sp全体レイアウト調整

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -2,12 +2,12 @@ import Headers from "@/components/headers";
 
 export default function AppLayout({ children }) {
   return (
-    <div className="flex flex-col justify-center items-center max-w-[800px]">
+    <div className="flex flex-col justify-center items-center max-w-[800px] min-h-screen">
       <Headers />
-      <main className="flex-grow">
+      <main className="flex-grow mt-12 p-8">
         {children}
       </main>
-      <footer className="text-white text-xl my-4 text-center w-full">©第5回RUNTEQ祭運営</footer>
+      <footer className="text-black my-4 text-center w-full">©第5回RUNTEQ祭運営</footer>
     </div>
   );
 }
--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -11,11 +11,11 @@ export default function Headers() {
       <header className={`fixed top-0 left-0 w-full text-blue-400 h-screen transition-all duration-500 ${openMenu ? "translate-x-0" : "delay-300 translate-x-full"} md:translate-x-0 md:h-auto`} >
         <nav className="bg-black w-full h-full bg-opacity-50 flex justify-center items-center md:bg-transparent">
           <ul className={`bg-white bg-opacity-40 w-1/2 ml-auto h-full flex flex-col justify-center items-start transition-all duration-500  ${openMenu ? "delay-300 translate-x-0" : "translate-x-full"} md:translate-x-0 md:flex-row md:items-center md:w-full md:shadow-lg`}>
-            <li className="w-full"><Link href="" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">全て</Link></li>
-            <li className="w-full"><Link href="" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">現役エンジニア</Link></li>
-            <li className="w-full"><Link href="" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">PF</Link></li>
-            <li className="w-full"><Link href="" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">ミニアプリ</Link></li>
-            <li className="w-full"><Link href="" className="bg-white block w-full pl-4 py-2 md:text-center md:py-4 md:bg-opacity-40 md:text-lg">チーム開発</Link></li>
+            <li className="w-full"><Link href="app" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">全て</Link></li>
+            <li className="w-full"><Link href="app#engineer" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">現役エンジニア</Link></li>
+            <li className="w-full"><Link href="app#pf" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">PF</Link></li>
+            <li className="w-full"><Link href="app#miniapp" className="bg-white block w-full pl-4 py-2 border-b border-orange-400 md:border-b-0 md:border-r md:text-center md:py-4 md:bg-opacity-40 md:text-lg">ミニアプリ</Link></li>
+            <li className="w-full"><Link href="app#team" className="bg-white block w-full pl-4 py-2 md:text-center md:py-4 md:bg-opacity-40 md:text-lg">チーム開発</Link></li>
           </ul>
         </nav>
       </header>
